449c55fdc44da79e54bd2d555ad9365441b56650
Juan Andres Mayorca <Jamayorca@gmail.com>
2024-08-17 01:26:19 +0200

    Pushable Objects
    + Added Pushable Objects with a test scene

--- scripts/hippo.gd
+++ scripts/hippo.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 class_name Hippo
 
-@export var speed: float = 100.0
+@export var speed: float = 400.0
+
 @export var maxWetness: float = 100.0
 @export var chargeDistance: float = 50.0
 
@@ -9,19 +10,19 @@ class_name Hippo
 @export var chargeTimer: Timer
 
 var currentWetness: float = 0.0
+var direction: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	currentWetness = maxWetness
 
 func _physics_process(delta: float) -> void:
-	var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	direction = Vector2.ZERO
+	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
 	if direction:
-		velocity = direction * (speed * 100) * delta
-	else:
-		velocity = Vector2.ZERO
-
-	move_and_slide()
+		velocity = direction * speed  * delta
+		move_and_collide(velocity)
+	
 
 func charge_ability() -> void:
 	if chargeTimer.is_stopped():
