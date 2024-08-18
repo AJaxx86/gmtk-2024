48ff26325e6b3059c6a86b7d172ee944ae187551
AJaxx86 <62261949+AJaxx86@users.noreply.github.com>
2024-08-18 01:18:36 +0100

    fuck dialogue

--- scripts/follow_camera.gd
+++ scripts/follow_camera.gd
@@ -3,6 +3,9 @@ class_name FollowCamera2D
 
 @export var target: Node2D
 
+func _ready() -> void:
+	add_to_group("Cameras")
+
 func _physics_process(delta: float) -> void:
 	if target:
 		global_position = target.global_position
