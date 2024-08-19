70b14856f00ac3e5209097974e713f01758fd454
AJaxx86 <62261949+AJaxx86@users.noreply.github.com>
2024-08-19 04:59:06 +0100

    completed grub bush

--- scripts/grub.gd
+++ scripts/grub.gd
@@ -7,27 +7,14 @@ signal CoughtGrub
 ##How far away the Grub will move.
 @export var moveRadius: float = 50.0
 
-@export_group("Nodes")
-@export var deathTimer: Timer
+@onready var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var lifespan: float
 var targetPos: Vector2
 
 func _ready() -> void:
 	targetPos = global_position + Vector2(randf_range(-moveRadius, moveRadius), randf_range(-moveRadius, moveRadius))
-	deathTimer.wait_time = lifespan
-	deathTimer.start()
-
-func _physics_process(delta: float) -> void:
-	if global_position != targetPos:
-		global_position = global_position.move_toward(targetPos, (speed * delta))
-	else:
-		targetPos = global_position + Vector2(randf_range(-moveRadius, moveRadius), randf_range(-moveRadius, moveRadius))
 
 func catch() -> void:
-	emit_signal("CoughtGrub")
-	die()
-
-func die() -> void:
-	print_debug("grub died")
+	GlobalStuff.add_item("grub")
 	queue_free()
