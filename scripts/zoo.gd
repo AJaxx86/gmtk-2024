0135960ea6aca3eab47c2cdf074d655ecc3cd0e5
AJaxx86 <62261949+AJaxx86@users.noreply.github.com>
2024-08-20 03:35:42 +0100

    added intro dialogue + music
    +added dialogue for first load of the zoo
    +music in the grub bush

--- scripts/zoo.gd
+++ scripts/zoo.gd
@@ -1,8 +1,14 @@
 extends Node2D
 class_name Zoo
 
+@export var prologueDialogue: Array[DialogueChunk]
+
 func _ready() -> void:
 	var player = get_tree().get_first_node_in_group("Player")
 	if player is Hippo:
 		player.global_position = GlobalStuff.hippoPosition
 		player.currentCharges = GlobalStuff.hippoCharges
+	
+	if GlobalStuff.firstLoad == true:
+		GlobalStuff.firstLoad = false
+		DialogueSystem.get_node("dialogue").start_dialogue(prologueDialogue)
